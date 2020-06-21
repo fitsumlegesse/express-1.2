c59f0d9c0591664531c7100cb72f1a5d849284c3
Fitsum Legesse <55632280+fitsumlegesse@users.noreply.github.com>
2020-06-20 22:00:30 -0700

    adds a data path

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require ('express');
-
+var data = require ('./data/test.json');
 var app = express();
 const PORT = 3000;
 
@@ -10,6 +10,7 @@ app.use(express.static(__dirname));
 // app.listen(3000);
 app.listen(PORT,()=>{
 console.log(`App is running on port ${PORT}`)
+console.log(data);
 
 
 });
