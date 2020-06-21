6672cacc35cc8ab28f42baee1e7b12e6844c44c9
Fitsum Legesse <55632280+fitsumlegesse@users.noreply.github.com>
2020-06-20 21:15:32 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 var express = require ('express');
+
 var app = express();
+
+//we can now serve up static pages
+app.use(express.static(__dirname));
+
+
+var app = express();
+
+
+
 app.listen(3000);
