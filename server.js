d434bc03cdacddd75d0e25962a6e16b5e4e366b5
Fitsum Legesse <55632280+fitsumlegesse@users.noreply.github.com>
2020-06-22 00:29:57 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,17 +6,18 @@ const PORT = 3000;
 // //we can now serve up static pages
 // app.use(express.static(__dirname));
 
+app.get('/',(req,res)=>{
+  res.send(`App is running on port ${PORT}`):
 
-// app.get(PORT,()=>{
-//   res.send(`App is running on port ${PORT}`);
-// });
+});
+
+
 
 
 // app.listen(3000);
 app.listen(PORT,()=>{
 console.log(`App is running on port ${PORT}`);
 console.log(data);
-
-
+  
 });
 
